d4977db9dc8d5cd4243070cce34eff3721219551
lei <3096407768@qq.com>
2023-02-07 16:10:59 +0800

    修改了设置定时推送的bug

--- schedule_scheduleJob.js
+++ schedule_scheduleJob.js
@@ -7,7 +7,6 @@ exports.schedule = () => {
     let time = new Date()
     let sendIs = false
 
-
     let j = schedule.scheduleJob({hour: data.hour, minute: data.min, second: 0}, function () {
         console.log("[INFO] 启动时间:", "-------->>", new Date().toLocaleString())
         sendIs = true
@@ -19,26 +18,33 @@ exports.schedule = () => {
 }
 //设定时间推送消息
 exports.setTime = (types, res, id) => {
-    res = res.split(",")
-    console.log(res)
-    if ((0 <= res[0] && res[0] <= 24) && (0 <= res[1] && res[1] <= 60)) {
-        if (res[2].length < 9) {
-            sendmessage.SendMessage(types, "QQ位数错误", id,)
-            return
-        }
+  try {
+      res = res.split(",")
+      console.log(res)
+      if ((0 <= res[0] && res[0] <= 24) && (0 <= res[1] && res[1] <= 60)) {
+          if (res[2].length < 9) {
+              sendmessage.SendMessage(types, "QQ位数错误", id,)
+              return
+          }
 
-        const data = {
-            hour: res[0],
-            min: res[1],
-            user_id: res[2],
-            data: res[3] + "(消息为qq机器人自动推送)",
-            types: "private",
-            id: id
+          const data = {
+              hour: res[0],
+              min: res[1],
+              user_id: res[2],
+              data: res[3] + "(消息为qq机器人自动推送)",
+              types: "private",
+              id: id
 
-        }
-        fs.writeFileSync("./QQbat/time.json", JSON.stringify(data))
-        sendmessage.SendMessage(types, "设置成功了，靓仔", id,)
-    } else {
-        sendmessage.SendMessage(types, "输入时间格式不对", id,)
-    }
+          }
+          fs.writeFileSync("./QQbat/time.json", JSON.stringify(data))
+          sendmessage.SendMessage(types, "设置成功了，靓仔", id,)
+      } else {
+          sendmessage.SendMessage(types, "输入时间格式不对", id,)
+      }
+  }catch (e) {
+      if(e){
+          sendmessage.SendMessage(types, "输入的,不对 ", id,)
+          fs.writeFileSync("./QQbat/time.json", "")
+      }
+  }
 }
