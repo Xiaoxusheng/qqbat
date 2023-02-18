a3d805fc31f13448ad039675529ae30e547c60bf
lei <3096407768@qq.com>
2023-02-18 12:07:19 +0800

    增加了新说明

--- getCass.js
+++ getCass.js
@@ -1,18 +1,18 @@
 const axios = require("axios")
 const {writeFileSync, readFileSync} = require("fs");
 const sendMessage = require("./send")
-const config=require("./config")
+const config = require("./config")
 let cookie = []
-let str = "",str1=""
+let str = "", str1 = ""
 
 //获取cookie
-async function getCookie(types, week, id){
+async function getCookie() {
     const res = await axios({
         method: "get",
         url: "https://passport2.chaoxing.com/api/login",
         params: {
-            name: 13277626850,
-            pwd: "lei125608"
+            // name: 账号,
+            // pwd:密码
         },
         withCredentials: true,
         headers: {
@@ -29,41 +29,20 @@ async function getCookie(types, week, id){
         }
     }
     console.log(cookie)
-   return function (){
-
-   }
-    // getclass(cookie, types, id, week)
+     getclass(cookie)
 
 
 }
 
 
-async function getclass(cookie, types, id, week) {
-    if (week < 2 || week > 19) {
-        sendMessage.SendMessage(types, "没课啊靓仔", id)
-        return
-    }
-    console.log(config.obj.week,week)
-    if (config.obj.week===week) {
-        let resp = JSON.parse(readFileSync("chet.json"))
-        console.log(resp)
-        let j = resp.sort((a, b) => {
-            return a.dayOfWeek - b.dayOfWeek
-        })
-        j.forEach(i => {
-            str1 += "周[" + i.dayOfWeek + "]" + "课程[" + i.name + "]" + "节次[" + i.beginNumber + "-" + (i.beginNumber + 1) + "]" + "教室[" + i.location + "]" + "任课教师[" + i.teacherName + "]" + "\n"
-            console.log("周[" + i.dayOfWeek + "]" + "课程[" + i.name + "]" + "节次[" + i.beginNumber + "-" + (i.beginNumber + 1) + "]" + "教室[" + i.location + "]" + "任课教师[" + i.teacherName + "]")
-        })
-        sendMessage.SendMessage(types, str1, id)
-        console.log(4444)
-        return
-    }
-    try{
+async function getclass(cookie) {
+
+    try {
         const {data: res} = await axios({
             method: "get",
             url: "https://kb.chaoxing.com/pc/curriculum/getMyLessons",
             params: {
-                week: week
+                week:2
             },
             withCredentials: true,
             headers: {
@@ -83,17 +62,15 @@ async function getclass(cookie, types, id, week) {
             // console.log(str += "周[" + i.dayOfWeek + "]" + "课程[" + i.name + "]" + "节次[" + i.beginNumber + "-" + (i.beginNumber + 1) + "]" + "教室[" + i.location + "]" + "任课教师[" + i.teacherName + "]" + "\n")
         })
         console.log(str)
-        sendMessage.SendMessage(types, str, id)
-        config.obj.week=week
-    }catch (e) {
-        sendMessage.SendMessage(types,"输入有误",id)
+
+    } catch (e) {
     }
 }
 
 
 
 
-
+getCookie()
 
 
 
--- ws.js
+++ ws.js
@@ -7,6 +7,8 @@ const ws = new WebSocketServer({port: 5700});
 exports.wsclint = () => {
     ws.on('connection', (socket) => {
         console.log('客户端连接成功')
+        const ip = req.socket.remoteAddress;
+        console.log("客户端IP：",ip)
         // 监听对方发送的消息
         socket.on('message', function message(res) {
             console.log(res.toString())
@@ -19,5 +21,8 @@ exports.wsclint = () => {
             console.log("推送服务器断开连接")
         })
 
+    })
+    ws.on("close",socket=>{
+        console.log(socket)
     })
 }
